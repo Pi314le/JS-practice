4e6537fc42efc605fb00015cba1bffdb1f2502a7
pineapple <1328990446@qq.com>
2025-04-24 00:06:42 +0800

    make delivery options interactive

--- 02-Amazon-Project/data/cart.js
+++ 02-Amazon-Project/data/cart.js
@@ -65,3 +65,18 @@ export function removeFromCart(productId) {
 
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  console.log(productId);
+
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
