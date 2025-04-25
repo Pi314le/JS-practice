19f5758fafdebd95ae4b6187d3cf238e48e45256
pineapple <1328990446@qq.com>
2025-04-25 23:57:52 +0800

    remove default tests and add money tests

---
+++ 02-Amazon-Project/tests-jasmine/money.test.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("converts cents into dollars", () => {
+    expect(formatCurrency(2095)).toBe("20.95");
+  });
+
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toBe("0.00");
+  });
+
+  it("rounds up to the nearest cen", () => {
+    expect(formatCurrency(2000.5)).toBe("20.01");
+  });
+});
