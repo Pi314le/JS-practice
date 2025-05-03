760d946afbd551efb4f0932841718c2d68860de2
pineapple <1328990446@qq.com>
2025-05-04 01:25:37 +0800

    learn promises and convert code from callback to promises (with loading products and cart from backend)

--- 02-Amazon-Project/scripts/checkout.js
+++ 02-Amazon-Project/scripts/checkout.js
@@ -2,15 +2,70 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/cart-oop.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-// send a request to the server to get the products data
-// and then ues a callback to wait for the response/ the data to be loaded
-// after the data is loaded,run the callback function = run the rest of the code
-loadProducts(() => {
+// wait for all promises to finish before going to the next step, instead of waiting for each promise to finish one by one
+Promise.all([
+  // give an array to add multiple promises to run/wait for in parallel
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then((values) => {
+  console.log(values); // ["value1", undefined]
+
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
 });
+
+/*
+// add some asynchronous code inner function of Promise function
+// when run some asynchronous code and then wait for it finish, and then call resolve() to go to the next step
+new Promise((resolve) => {
+  // console.log("start promise");
+
+  // some asynchronous code
+  loadProducts(() => {
+    // console.log("finished loading");
+
+    resolve("value1"); // call resolve() to go to the next step when loadProducts() is finished
+  });
+})
+  .then((value) => {
+    console.log(value);
+
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve(); // give/call a function resolve() to go to the next step when loadCart() is finished
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  });
+*/
+
+/*
+// send a request to the server to get the products data
+// and then ues a callback to wait for the response/ the data to be loaded
+// after the data is loaded,run the callback function = run the rest of the code
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  });
+});
+*/
